4639cca99de5f3050c2188ade581ff68fdefb1c2
João Gondim <joao.gondim@atos.net>
2020-01-30 12:01:29 -0300

    saving changes

--- frametotext.py
+++ frametotext.py
@@ -3,6 +3,26 @@ from PIL import Image
 import glob
 import os
 import json
+import joblib
+from sklearn import datasets
+from skimage.feature import hog
+from sklearn.svm import LinearSVC
+import numpy as np
+
+'''dataset = datasets.load_digits()
+features = np.array(dataset.data, 'int16')
+labels = np.array(dataset.data, 'int')
+
+list_hog_fd = []
+for feature in features:
+    fd = hog(feature.reshape((64, 64)), orientations=9, pixels_per_cell=(14, 14), cells_per_block=(1, 1), visualise=False)
+    list_hog_fd.append(fd)
+hog_features = np.array(list_hog_fd, 'float64')
+
+clf = LinearSVC()
+clf.fit(hog_features, labels)
+joblib.dump(clf, "digits_cls.pkl", compress=3)
+clf = joblib.load("digits_cls.pkl")'''
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
 
@@ -13,7 +33,7 @@ def get_video_begin_end(video, framerate):
     path_to_file = 'frames/' + video[filenamestart:filenameend - 1]
     framecount = framerate
     for i in sorted(glob.glob(path_to_file + '/frameBegin*.jpg'), key=os.path.getmtime):
-        text = pytesseract.image_to_string(Image.open(i), lang='ENG')
+        text = pytesseract.image_to_string(Image.open(i), lang='perfect')
         if 'frameBegin0.jpg' in i:
             date_begin = text[text.find('-') - 2 : text.find('-') + 8]
             begin_time = text[text.find(':') - 2 : text.find(':') + 6]
@@ -26,7 +46,7 @@ def get_video_begin_end(video, framerate):
             framecount -= 1
     framecount = 0
     for i in sorted(glob.glob(path_to_file + '/frameEnd*.jpg'), key=os.path.getmtime):
-        text = pytesseract.image_to_string(Image.open(i), lang='ENG')
+        text = pytesseract.image_to_string(Image.open(i), lang='perfect')
         if 'frameEnd0.jpg' in i:
             date_end = text[text.find('-') - 2: text.find('-') + 8]
             end_time = text[text.find(':') - 2: text.find(':') + 6]
--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import getFrames as gf
+import glob
 import frametotext as ftt
 
 if __name__ == '__main__':
-    gf.get_video_frames('../videos_convertidos/ch0019_00000001575014401.mp4', 8)
-    ftt.get_video_begin_end('../videos_convertidos/ch0019_00000001575014401.mp4', 8)
+    videopath = glob.glob('../videos_convertidos/ch0019_00000001575014401.mp4')
+    gf.get_video_frames(videopath[0], 8)
+    ftt.get_video_begin_end(videopath[0], 8)
